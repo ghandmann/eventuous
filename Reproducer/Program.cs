979e534c4e80d58511a389ce40ca903683d95f48
Sven Eppler <git@sveneppler.de>
2021-12-11 22:50:22 +0100

    add some commentary
    Signed-off-by: Sven Eppler <git@sveneppler.de>

--- Reproducer/Program.cs
+++ Reproducer/Program.cs
@@ -7,6 +7,7 @@ using Eventuous.Sut.Domain;
 using Microsoft.Extensions.DependencyInjection;
 using NodaTime;
 
+// Setup DI container
 var collection = new ServiceCollection();
 collection.AddSingleton(new EventStoreClient(
     EventStoreClientSettings.Create("esdb://localhost:2113?tls=false")
@@ -21,18 +22,23 @@ collection.AddSingleton<IAggregateStore, AggregateStore>();
 
 var services = collection.BuildServiceProvider();
 
+// Get an aggregate store
 var store = services.GetRequiredService<IAggregateStore>();
 
 BookingEvents.MapBookingEvents();
 
 var booking = new Booking();
 
+// Create a booking
 var bookingId = new BookingId(Guid.NewGuid().ToString());
 var stayOneNight = new StayPeriod(new LocalDate(2021,1, 1), new LocalDate(2021,1,2));
 booking.BookRoom(bookingId, "B12", stayOneNight, 100);
 
+// Store the booking
 await store.Store(booking, CancellationToken.None);
 
+// just load the booking back from the store.
+// typically fails after less than 20 iterations
 for(int i = 0; i < 100; i++) {
     var replayedBooking = await store.Load<Booking>(bookingId, CancellationToken.None);
     Console.WriteLine($"Iteration: {i}. replayedBooking.State.Id={replayedBooking.State.Id}");
