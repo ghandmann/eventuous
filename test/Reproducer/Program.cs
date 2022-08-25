b13d92435e4bce3e2bbfcd7ee6cd8d722fd606ba
Sven Eppler <sven.eppler@sodgeit.de>
2022-08-25 08:33:29 +0200

    unit test reproducer

---
+++ test/Reproducer/Program.cs
@@ -0,0 +1,10 @@
+﻿using Eventuous.Sut.Domain;
+using NodaTime;
+
+var stayPeriod = new StayPeriod(new LocalDate(2022, 01, 01), new LocalDate(2022, 01, 10));
+
+var booking = new Booking();
+
+booking.BookRoom("1", stayPeriod, 100, "some-guest");
+
+booking.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
