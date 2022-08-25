f89401ea2c1513c7896b20bce4789b21151943b3
Sven Eppler <sven.eppler@sodgeit.de>
2022-08-25 08:36:22 +0200

    show workaround

--- test/Reproducer/Program.cs
+++ test/Reproducer/Program.cs
@@ -4,7 +4,18 @@ using NodaTime;
 var stayPeriod = new StayPeriod(new LocalDate(2022, 01, 01), new LocalDate(2022, 01, 10));
 
 var booking = new Booking();
-
 booking.BookRoom("1", stayPeriod, 100, "some-guest");
-
+// Next line throws, since RecordPayment runs 'EnsureExists' while the 'Aggregate.Original' property still indicates a non existing aggregate
 booking.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
+
+
+
+// Workaround with 'Load()'
+
+var booking2 = new Booking();
+booking2.BookRoom("1", stayPeriod, 100, "some-guest");
+
+// Backfeeding the changes to the aggregate works
+booking2.Load(booking2.Changes);
+
+booking2.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
