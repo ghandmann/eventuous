c7a12d5ec5c0c0c9cfcbcb843eb12a45b33a84b9
Sven Eppler <sven.eppler@sodgeit.de>
2022-08-25 08:45:19 +0200

    fine tune

--- test/Reproducer/Program.cs
+++ test/Reproducer/Program.cs
@@ -5,7 +5,7 @@ var stayPeriod = new StayPeriod(new LocalDate(2022, 01, 01), new LocalDate(2022,
 
 var booking = new Booking();
 booking.BookRoom("1", stayPeriod, 100, "some-guest");
-// Next line throws, since RecordPayment runs 'EnsureExists' while the 'Aggregate.Original' property still indicates a non existing aggregate
+// Next line throws, since RecordPayment calls 'EnsureExists' while the 'Aggregate.Original' property still indicates a non existing aggregate
 booking.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
 
 
@@ -15,7 +15,7 @@ booking.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
 var booking2 = new Booking();
 booking2.BookRoom("1", stayPeriod, 100, "some-guest");
 
-// Backfeeding the changes to the aggregate works
+// Backfeeding the changes to the aggregate works, but feels wrong
 booking2.Load(booking2.Changes);
 
 booking2.RecordPayment("payment-1234", 50, DateTimeOffset.Now);
